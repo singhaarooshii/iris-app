2a46786413422259d6d4a08d3a82f01dbb3f9ea8
Arushi Singh <saarushi12.03@gmail.com>
2026-07-08 13:10:21 +0530

    Fix slider labels for Iris Flower measurements

--- myapp.py
+++ myapp.py
@@ -10,9 +10,9 @@ st.title("Iris Flower Classifier")
 st.write("Enter the measurements below:")
 #input sliders
 sepal_length=st.slider("Sepal Length (cm)" , 4.0,8.0,5.8)
-sepal_width=st.slider("Sepal Length (cm)" , 2.0,4.5,3.0)
-petal_length=st.slider("Sepal Length (cm)" , 1.0,7.0,4.0)
-petal_width=st.slider("Sepal Length (cm)" , 0.1,2.5,1.2)
+sepal_width=st.slider("Sepal Width (cm)" , 2.0,4.5,3.0)
+petal_length=st.slider("Petal Length (cm)" , 1.0,7.0,4.0)
+petal_width=st.slider("Petal Width (cm)" , 0.1,2.5,1.2)
 #prediction
 features = np.array([[sepal_length,sepal_width,petal_length,petal_width]])
 
